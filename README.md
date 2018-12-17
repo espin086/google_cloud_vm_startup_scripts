52fe9676fee4b010b7153dd1527a46885e667157
jj_espinoza_la <jj_espinoza_la@development.c.jj-projects-210105.internal>
2018-12-17 22:01:17 +0000

    added details to readme

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # google_cloud_vm_startup_scripts
 
 ## Introduction
+Creating Google Cloud Virtual Machines is a fairly easy process.  This repository contains 
+shell scrips (Linux) that installs software for data science purposes.  
+
 
 ## Packages Installed
+
+* Git - for version control
+* Anaconda - contains Python, R, and useful Python libraries
+* Tree - useful for visualizing a Linux directory
+
+
+## How to Run
+1. Simply creat a Google Virtual Machine, git clone the repository found here: https://github.com/espin086/google_cloud_vm_startup_scripts
+2. Run the shell script contained in the repository by typing `sudo sh google_vm_startup.sh`
+
+
+
+
+
 
 
 
--- gcs_vm_startup.sh
+++ gcs_vm_startup.sh
@@ -1,12 +1,15 @@
 echo '*************************************************************************'
 echo 'INFO: installing git'
 echo '*************************************************************************'
+#installs git
 apt-get install git
+#saves credentials so github doesn't ask you everytime something is updated
+git config credential.helper store
 
 echo '*************************************************************************'
 echo 'INFO: installing useful utilities'
 echo '*************************************************************************'
-echo 'INFO: installing useful shell utilities'
+echo 'INFO: installing useful shell utilities't config credential.helper store
 git clone https://github.com/espin086/google_cloud_vm_startup_scripts.git
 
 
