fda65ac42484240f15abe1c82453197786cc53d2
jj_espinoza_la <jj_espinoza_la@development.c.jj-projects-210105.internal>
2018-12-19 05:32:25 +0000

    added tree package and cleaned up Anaconda install

--- gcs_vm_startup.sh
+++ gcs_vm_startup.sh
@@ -9,7 +9,15 @@ git config credential.helper store
 echo '*************************************************************************'
 echo 'INFO: installing useful utilities'
 echo '*************************************************************************'
-echo 'INFO: installing useful shell utilities't config credential.helper store
-git clone https://github.com/espin086/google_cloud_vm_startup_scripts.git
+
+echo 'INFO: installing anaconda'
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+sudo apt-get install bzip2
+sh Anaconda3-5.3.1-Linux-x86_64.sh -u
+rm Anaconda3-5.3.1-Linux-x86_64.sh
+echo 'INFO: You may need type in the follwing to update the path of python to\
+	Anaconda version: export PATH="/home/jj_espinoza_la/anaconda3/bin:$PATH"'
 
 
+echo 'INFO: installing tree Linux package'
+sudo apt-get install tree
